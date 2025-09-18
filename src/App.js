53e3b387b8a566228934e79536059dab81a6d915
liviucozac <liviucozac28@gmail.com>
2025-09-18 23:47:29 +0300

    Add global sticky header with logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Scanner from "./components/Scanner";
 import ProductDetails from "./pages/ProductDetails";
+import './App.css';
 
 function App() {
   return (
     <Router>
+      {/* Global Logo/Header */}
+      <header className="app-header">
+        <img src="/logo.jpg" alt="App Logo" className="app-logo" />
+      </header>
+
       <Routes>
         {/* Home = Scanner directly */}
         <Route path="/" element={<Scanner />} />
